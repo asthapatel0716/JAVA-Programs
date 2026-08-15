0c99ad0e7d30cc087046dda169600e79e699fb36
asthapatel0716 <asthapatel0716@gmail.com>
2026-08-15 15:41:22 +0530

    Update LoopPatterns.java

--- LoopPatterns.java
+++ LoopPatterns.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class LoopPatterns {
     public static void patternA(int size) {
         for (int i = 1; i <= size; i++) {
@@ -38,5 +39,25 @@ public class LoopPatterns {
             }
             System.out.println();
         }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size: ");
+        int size = sc.nextInt();
+        if (size < 1) {
+            System.out.println("Invalid size.");
+        } else {
+            System.out.println("\nPattern A:");
+            patternA(size);
+            System.out.println("\nPattern B:");
+            patternB(size);
+            System.out.println("\nPattern C:");
+            patternC(size);
+            System.out.println("\nPattern D:");
+            patternD(size);
+            System.out.println("\nPattern E:");
+            patternE(size);
+        }
+        sc.close();
     }
 }
