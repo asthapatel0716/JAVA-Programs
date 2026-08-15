1be42dc3cf28f1e1e59c2bcd1ab43123373b184b
asthapatel0716 <asthapatel0716@gmail.com>
2026-08-15 10:29:09 +0530

    Create GreatestCommonDivisor.java

---
+++ GreatestCommonDivisor.java
@@ -0,0 +1,12 @@
+public class GreatestCommonDivisor {
+    public static int gcd(int first, int second) {
+        first = Math.abs(first);
+        second = Math.abs(second);
+        while (second != 0) {
+            int remainder = first % second;
+            first = second;
+            second = remainder;
+        }
+        return first;
+    }
+}
