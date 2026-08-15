061f027a9865055e7080e7a7caa71af6fa9131e4
asthapatel0716 <asthapatel0716@gmail.com>
2026-08-15 15:27:57 +0530

    Create LeapYear.java

--- LeapYear.java
+++ LeapYear.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
 public class LeapYear {
-
     public static boolean isLeapYear(int year) {
-        return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+        return (year % 400 == 0) ||
+               (year % 4 == 0 && year % 100 != 0);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter year: ");
+        int year = sc.nextInt();
+        if (isLeapYear(year)) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+        sc.close();
     }
 }
