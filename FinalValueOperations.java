23ef7857a9bc8549b83cf6bfcebbb625cd4094b6
asthapatel0716 <asthapatel0716@gmail.com>
2026-08-15 10:54:40 +0530

    Create FinalValueOperations.java

---
+++ FinalValueOperations.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int x = 0;
+        for (String operation : operations) {
+            if (operation.contains("+")) {
+                x++;
+            } else {
+                x--;
+            }
+        }
+        return x;
+    }
+}
