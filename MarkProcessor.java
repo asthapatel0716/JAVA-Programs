fef82f6993e4b55ac8a87d64e20e7813f36baccc
asthapatel0716 <asthapatel0716@gmail.com>
2026-08-15 15:45:17 +0530

    Update MarkProcessor.java

--- MarkProcessor.java
+++ MarkProcessor.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class MarksProcessor {
     public static boolean isValidMark(int mark) {
         return mark >= 0 && mark <= 100;
@@ -13,5 +14,42 @@ public class MarksProcessor {
         System.out.println("Highest mark: " + highest);
         System.out.println("Number of passes: " + passes);
         System.out.println("Number of failures: " + failures);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int mark;
+        int count = 0;
+        int sum = 0;
+        int highest = 0;
+        int passes = 0;
+        int failures = 0;
+        do {
+            System.out.print("Enter mark (-1 to stop): ");
+            mark = sc.nextInt();
+            if (mark == -1) {
+                break;
+            }
+            if (!isValidMark(mark)) {
+                System.out.println("Invalid mark. Enter a mark from 0 to 100.");
+                continue;
+            }
+            count++;
+            sum += mark;
+            if (mark > highest) {
+                highest = mark;
+            }
+            if (mark >= 50) {
+                passes++;
+            } else {
+                failures++;
+            }
+        } while (mark != -1);
+        if (count > 0) {
+            double average = calculateAverage(sum, count);
+            printResults(count, sum, average, highest, passes, failures);
+        } else {
+            System.out.println("No valid marks were entered.");
+        }
+        sc.close();
     }
 }
