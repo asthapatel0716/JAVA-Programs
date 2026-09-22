a18ddc1137cf3d3105ce9543ecf6db9690bd5227
asthapatel0716 <asthapatel0716@gmail.com>
2026-09-22 21:04:07 +0530

    Create PersonHeirarchy.java

---
+++ Week5_Assignments/Week7_Assignments/PersonHeirarchy.java
@@ -0,0 +1,39 @@
+class Person {
+    void describeRole() {
+        System.out.println("I am a person.");
+    }
+}
+class Student extends Person {
+    @Override
+    void describeRole() {
+        System.out.println("I am a student.");
+    }
+    void submitAssignment() {
+        System.out.println("Student is submitting an assignment.");
+    }
+}
+class Instructor extends Person {
+    @Override
+    void describeRole() {
+        System.out.println("I am an instructor.");
+    }
+    void conductClass() {
+        System.out.println("Instructor is conducting a class.");
+    }
+}
+public class PersonHierarchy {
+    public static void main(String[] args) {
+        Person[] people = { new Student(), new Instructor(), new Student() };
+        for (Person p : people) {
+            p.describeRole();
+        }
+        for (Person p : people) {
+            if (p instanceof Student student) {
+                student.submitAssignment();
+            }
+            if (p instanceof Instructor instructor) {
+                instructor.conductClass();
+            }
+        }
+    }
+}
