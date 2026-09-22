4f9ac10370891728b68f8300578cad02a157eeaf
asthapatel0716 <asthapatel0716@gmail.com>
2026-09-22 20:05:02 +0530

    Update ShapeManagementApp.java

--- Week5_Assignments/ShapeManagementApp.java
+++ Week5_Assignments/ShapeManagementApp.java
@@ -1,75 +1,85 @@
-class Shape {
-    private String name;
-    Shape(String name) {
-        this.name = name;
+abstract class Shape {
+    private String color;
+    Shape(String color) {
+        this.color = color;
     }
-    public String toString() {
-        return "Shape: " + name;
+    public String getColor() {
+        return color;
     }
+    public abstract double area();
+    public abstract double perimeter();
 }
 class Circle extends Shape {
     private double radius;
-    Circle(String name, double radius) {
-        super(name);
+    Circle(String color, double radius) {
+        super(color);
+        if (radius <= 0) {
+            System.out.println("Invalid dimensions.");
+        }
+
         this.radius = radius;
     }
-    double area() {
-        return Math.PI * radius * radius;
-    }
-    double perimeter() {
-        return 2 * Math.PI * radius;
+    @Override
+    public double area() {
+        return 3.14 * radius * radius;
     }
     @Override
-    public String toString() {
-        return "Circle: Radius = " + radius + ", Area = " + area() + ", Perimeter = " + perimeter();}
+    public double perimeter() {
+        return 2 * 3.14 * radius;
+    }
 }
 class Rectangle extends Shape {
     private double length;
     private double width;
-    Rectangle(String name, double length, double width) {
-        super(name);
+    Rectangle(String color, double length, double width) {
+        super(color);
+        if (length <= 0 || width <= 0) {
+            System.out.println("Invalid dimensions.");
+        }
         this.length = length;
         this.width = width;
     }
-    double area() {
+    @Override
+    public double area() {
         return length * width;
     }
-    double perimeter() {
-        return 2 * (length + width);
-    }
     @Override
-    public String toString() {
-        return "Rectangle: Length = " + length + ", Width = " + width + ", Area = " + area() + ", Perimeter = " + perimeter();
+    public double perimeter() {
+        return 2 * (length + width);
     }
 }
 class Triangle extends Shape {
     private double base;
     private double height;
-    Triangle(String name, double base, double height) {
-        super(name);
+    private double side1;
+    private double side2;
+    private double side3;
+    Triangle(String color, double base, double height,
+             double side1, double side2, double side3) {
+        super(color);
+        if (base <= 0 || height <= 0 || side1 <= 0 || side2 <= 0 || side3 <= 0) {
+            System.out.println("Invalid dimensions.");
+        }
         this.base = base;
         this.height = height;
-    }
-    double area() {
-        return (base * height) / 2;
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
     }
     @Override
-    public String toString() {
-        return "Triangle: Base = " + base + ", Height = " + height + ", Area = " + area();
+    public double area() {
+        return 0.5 * base * height;
+    }
+    @Override
+    public double perimeter() {
+        return side1 + side2 + side3;
     }
 }
 public class ShapeManagementApp {
-    public static void main(String[] args) {
-        Circle c1 = new Circle("Circle 1", 5);
-        Rectangle r1 = new Rectangle("Rectangle 1", 4, 6);
-        Triangle t1 = new Triangle("Triangle 1", 3, 4);
-        System.out.println(c1);
-        System.out.println("Area = " + c1.area());
-        System.out.println("Perimeter = " + c1.perimeter());
-        System.out.println(r1);
-        System.out.println("Area = " + r1.area());
-        System.out.println("Perimeter = " + r1.perimeter());
-        System.out.println(t1);
-        System.out.println("Area = " + t1.area());
+        public static void main(String[] args) {
+        Shape[] shapes = {new Circle("Red", 5), new Rectangle("Blue", 6, 4), new Triangle("Green", 6, 4, 5, 5, 6), new Circle("Yellow", 3), new Rectangle("Purple", 8, 3) };
+        for (Shape shape : shapes) {
+            System.out.println(shape);
+        }
     }
 }
